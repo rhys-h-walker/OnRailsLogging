0498dc104a11bb4b489b25a4e7c08d5803a34870
Rhysw <rw531@kent.ac.uk>
2025-07-15 22:37:14 +0100

    Removed unused imports

--- src/main/java/com/github/rhys_h_walker/FileManagement.java
+++ src/main/java/com/github/rhys_h_walker/FileManagement.java
@@ -1,9 +1,6 @@
 package com.github.rhys_h_walker;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
