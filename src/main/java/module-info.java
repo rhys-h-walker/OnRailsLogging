c55361fad053aee250fbc79ad55ffcaf974430c5
Rhysw <rw531@kent.ac.uk>
2025-07-29 19:18:34 +0100

    Updated to now feature a module descriptor for use with module java projects

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.github.rhys_h_walker.OnrailsLogging {
+    exports com.github.rhys_h_walker.factories;
+    exports com.github.rhys_h_walker.core_enums;
+    exports com.github.rhys_h_walker;
+    exports com.github.rhys_h_walker.misc;
+    exports com.github.rhys_h_walker.core_objects;
+}
